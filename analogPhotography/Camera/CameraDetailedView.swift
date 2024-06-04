c1a37d7de3840b53cda4061180403d1ea114678d
Hurynovič Andrej <dpkdpk10@icloud.com>
2024-06-05 01:14:42 +0300

    Add CameraDetailedView.

---
+++ analogPhotography/Camera/CameraDetailedView.swift
@@ -0,0 +1,64 @@
+//
+//  CameraDetailedView.swift
+//  analogPhotography
+//
+//  Created by Andrej Hurynovič on 4.06.24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct CameraDetailedView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Bindable var camera: Camera
+    
+    var body: some View {
+        List {
+            
+            Section("Информация") {
+                TextFieldFormView(title: "Название:", placeholder: "добавить", text: $camera.name)
+            }
+            
+            Section("Плёнки") {
+                Button("Добавить", systemImage: "plus.circle.fill", action: {
+                    if camera.films.isEmpty == false {
+//                        FilmsView(films: films)
+                    }
+                })
+            }
+            
+            Section("Заметки") {
+                TextField("Добавить замтетку", text: $camera.note, axis: .vertical)
+            }
+        }
+        .navigationTitle(camera.name)
+        .navigationBarTitleDisplayMode(.large)
+        .toolbar { toolbarMenu }
+        
+    }
+    
+    var toolbarMenu: some View {
+        Menu("", systemImage: "ellipsis.circle") {
+            DeleteButton {
+                modelContext.delete(camera)
+            }
+        }
+    }
+    
+}
+
+
+#Preview {
+    NavigationView {
+        do {
+            let config = ModelConfiguration(isStoredInMemoryOnly: true)
+            let container = try ModelContainer(for: Camera.self,
+                                               configurations: config)
+            let previewCamera = Camera(name: "Pentax SFX", note: "Мой личный Pentax SFX", Films: [])
+            return CameraDetailedView(camera: previewCamera)
+                .modelContainer(container)
+        } catch {
+            fatalError("Failed to create model")
+        }
+    }
+}
