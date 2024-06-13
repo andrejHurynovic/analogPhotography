8516d874f8ff7ac40aadc654130ff9c7e4ae271e
Hurynovič Andrej <dpkdpk10@icloud.com>
2024-06-13 13:59:32 +0300

    Add editState in TextFieldFormView

--- analogPhotography/Generics and templates/TextFieldFormView.swift
+++ analogPhotography/Generics and templates/TextFieldFormView.swift
@@ -8,22 +8,43 @@
 import SwiftUI
 
 struct TextFieldFormView: View {
-    
     var title: String
     var placeholder: String
+    
     @Binding var text: String
+    @Binding var editState: Bool
+    
+    @FocusState var focusState: Bool
+    
+    var foregroundColor: Color { !focusState ? .gray : .primary }
     
     var body: some View {
         FormView(title: title) {
             TextField(placeholder, text: $text, axis: .horizontal)
                 .multilineTextAlignment(.trailing)
+                .foregroundStyle(foregroundColor)
             
+                .onSubmit {
+                    editState = false
+                }
+            
+                .focused($focusState)
+                .onChange(of: focusState) { _, newValue in
+                    editState = newValue
+                }
         }
+        
     }
 }
 
 #Preview {
     @State var text: String = ""
+    @State var editState: Bool = true
     
-    return TextFieldFormView(title: "Название", placeholder: "добавить", text: $text)
+    return List {
+        TextFieldFormView(title: "Название",
+                          placeholder: "Добавить",
+                          text: $text,
+                          editState: $editState)
+    }
 }
